9434cd164d52724e92d019b2eb39e326d0dbb304
edwardlanto <edwardlanto.developer@gmail.com>
2022-06-27 08:21:54 -0700

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Layout from "../components/layouts/Layout";
+import Hero from '../components/sections/Hero/index';
+
+function Home() {
+	return (
+		<>
+			<Layout>
+				<Hero />
+			</Layout >
+		</>
+	);
+}
+
+export default Home;
